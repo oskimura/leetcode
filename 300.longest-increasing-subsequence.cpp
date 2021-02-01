226d060ba3fabaf9494a7b215f1559f9783a85de
oskimura <oskimura@gmail.com>
2021-02-02 02:27:50 +0900

    Add Longest Increasing Subsequence

--- 300.longest-increasing-subsequence.cpp
+++ 300.longest-increasing-subsequence.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+
+using namespace std;
+// @before-stub-for-debug-end
+
+
 /*
  * @lc app=leetcode id=300 lang=cpp
  *
@@ -6,10 +14,19 @@
 
 // @lc code=start
 class Solution {
+    const int INF = 1e9+1;
 public:
     int lengthOfLIS(vector<int>& nums) {
-        
-        
+        int n = nums.size();
+
+        vector<int>dp(n+1,INF);
+        for (int i = 0; i < n; i++)
+        {
+            int x = nums[i];
+            auto tmp = lower_bound(dp.begin(),dp.end(),x);
+            *tmp = x;
+        }        
+        return (lower_bound(dp.begin(),dp.end(),INF) - dp.begin());
     }
 };
 // @lc code=end
